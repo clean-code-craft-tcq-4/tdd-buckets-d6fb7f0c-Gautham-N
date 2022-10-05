1275f3b669943d1b61d54a173015050a72f645cf
Gautham <58910261+Gautham-N@users.noreply.github.com>
2022-10-05 18:10:11 +0530

    Update TddTestFunction.cpp

--- TddTestFunction.cpp
+++ TddTestFunction.cpp
@@ -3,7 +3,7 @@
 
 bool SampleRangeReadings(unsigned int Current_Samples[], int Current_Samples_size, int ReadingsCount)
 {
-    unsigned int GetReadingsCount = ConsecutiveRanges(Current_Samples, Current_Samples_size);
+    int GetReadingsCount = ConsecutiveRanges(Current_Samples, Current_Samples_size);
 
     if(GetReadingsCount == ReadingsCount)
     {
